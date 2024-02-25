3eac5f94defdb975a760ab3778e76b92d6fc6d8d
Venture <wcdeng0331@gmail.com>
2024-02-26 00:35:21 +0800

    加入RTOS

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,36 @@
-#include <Arduino.h>
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_system.h"
+#include "esp_spi_flash.h"
 
-// put function declarations here:
-int myFunction(int, int);
-
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+// 任务1执行的函数
+void task1(void *pvParameters) {
+    while (1) {
+        printf("Hello ");
+        vTaskDelay(1000 / portTICK_PERIOD_MS); // 延迟1秒
+    }
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+// 任务2执行的函数
+void task2(void *pvParameters) {
+    while (1) {
+        printf("World!\n");
+        vTaskDelay(1000 / portTICK_PERIOD_MS); // 延迟1秒
+    }
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void app_main(void) {
+    // 创建任务1
+    xTaskCreate(&task1, "Task 1", 2048, NULL, 1, NULL);
+    // 创建任务2
+    xTaskCreate(&task2, "Task 2", 2048, NULL, 1, NULL);
+}
+
+void setup(){
+  app_main();
+}
+
+void loop(){
+  
 }
